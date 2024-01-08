6984124d5c4bc243e2a8b144c5a53ccd3aa00e53
nathanthomp <nathanthomp30@gmail.com>
2024-01-08 00:06:17 -0500

    Implementation of get_token

--- compiler/src/main.c
+++ compiler/src/main.c
@@ -10,6 +10,7 @@ typedef enum {
     UNSUPPORTED_TOKEN,
     NUMBER_TOKEN,
     BLANK_TOKEN,
+	LETTER_TOKEN,
     PLUS_TOKEN,
     MINUS_TOKEN,
     STAR_TOKEN,
@@ -25,81 +26,71 @@ typedef struct {
     char text;
 } Token;
 
-Token *next_token(int *pos, const char *input) { 
-    if (*pos >= strlen(input))
-        return NULL;
-    /* pos < input.len */
-    Token *tok = (Token*)malloc(sizeof(Token));
-    char current = input[*pos];
-    if (isalpha(current)) {
-        switch (current) {
-            case '+':
-                tok->type = PLUS_TOKEN;
-                break;
-            case '-':
-                tok->type = MINUS_TOKEN;
-                break;
-            case '*':
-                tok->type = STAR_TOKEN;
-                break;
-            case '/':
-                tok->type = SLASH_TOKEN;
-                break;
-            case '{':
-                tok->type = LBRACK_TOKEN;
-                break;
-            case '}':
-                tok->type = RBRACK_TOKEN;
-                break;
-            case '(':
-                tok->type = LPAREN_TOKEN;
-                break;
-            case ')':
-                tok->type = RPAREN_TOKEN;
-                break;
-            default:
-                /* Error unsupported token */
-                tok->type = UNSUPPORTED_TOKEN;
-                break;
-        }
-        tok->text = current;
-    } else if (isdigit(current)) {
-        /* Numeric */
-        tok->type = NUMBER_TOKEN;
-        tok->text = current;
-        //while (isdigit(input[(*pos)++]))
-        //    tok->text = buff;
-    }
-    else if (isblank(current)) {
-        tok->type = BLANK_TOKEN;
-        tok->text = current;
-    }
-    else {
-        /* Error unknown token */
-        tok->type = UNKNOWN_TOKEN;
-        tok->text = current;
-    }
-    (*pos)++;
-}
+Token *next_token(int *pos, const char *input) {
+	
+	// To print the pointer: *pos
+	// To increment the pointer: (*pos)++;
+	// printf("pos = %d\n", *pos);
+	// printf("strlen(input) = %ld\n", strlen(input));
 
-void tokenize(const char *input) {
-    int pos = 0;
-    Token *tok = next_token(&pos, input);
-    do {
-        printf("Token:%c\tType:%d\n", tok->text, tok->type);
-        tok = next_token(&pos, input);
-    } while (tok);
-}
+	// Number
+	// Operator
+ 
+	Token *tok = (Token*)malloc(sizeof(Token));
+	char current = input[*pos];
+	tok-> text = current;
 
-char input[INPUT_BUFF_SIZE];
+	if (isdigit(current)) {
+		tok->type = NUMBER_TOKEN;
+	} else if (isalpha(current)) { 
+		switch (current) {
+			case '+':
+				tok->type = PLUS_TOKEN;
+				break;
+			case '-':
+				tok->type = MINUS_TOKEN;
+				break;
+			case '*':
+				tok->type = STAR_TOKEN;
+				break;
+			case '/':
+				tok->type = SLASH_TOKEN;
+				break;
+			case '{':
+				tok->type = LBRACK_TOKEN;
+				break;
+			case '}':
+				tok->type = RBRACK_TOKEN;
+				break;
+			case '(':
+				tok->type = LPAREN_TOKEN;
+				break;
+			case ')':
+				tok->type = RPAREN_TOKEN;
+				break;
+			default:
+				tok->type = LETTER_TOKEN;
+				break;
+		}
+	} else {
+		tok->type = UNKNOWN_TOKEN;
+	}
+
+	(*pos)++;
+	return tok;
+}
 
 int main(int argc, char **argv) {
-    /* REPL */ 
-    printf("NSquared\n");
+	char input[INPUT_BUFF_SIZE];
     while (1) {
-        fputs("ns> ", stdout);
+        printf("ns> ");
         fgets(input, INPUT_BUFF_SIZE, stdin);
-        tokenize(input); 
+
+		int pos = 0;
+		while (input[pos] != '\n') {
+			Token *tok = next_token(&pos, input);
+			printf("Token:%c\tType:%d\n", tok->text, tok->type);
+		}
     }
     return 0;
 }
